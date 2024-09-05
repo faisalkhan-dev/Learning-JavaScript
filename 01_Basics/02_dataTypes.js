f611588e832defe0efb9b96abd02b5c2d48e1229
Faisal Khan <faisal5189126@gmail.com>
2024-09-05 16:55:16 +0530

    learned datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; //used treat whole code as per new version of js
+
+// number=2
+// bigint=
+// string=> ""
+// boolean=true/false
+// null=standalone value 
+// undefined=>
+// symbol=>unique
+
+console.log(typeof null)            //object
+console.log(typeof undefined)       //undefined
