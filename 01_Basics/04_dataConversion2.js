232ba128b18653dc863a7a9a9a81f1f438d854a5
Faisal Khan <faisal5189126@gmail.com>
2024-09-06 23:00:28 +0530

    DataConversion part2

---
+++ 01_Basics/04_dataConversion2.js
@@ -0,0 +1,20 @@
+console.log("2"+3)   //23
+console.log(2+"3")    //23
+console.log(2+3)        //5
+console.log("2"+"3")       //23
+console.log("concatenation of string and numbers")
+console.log(1+2+"3")    //33
+console.log("1"+2+3)    //123
+console.log(1+"2"+3)    //123
+
+console.log(+true) //1 as there is a data conversion in increment operator
+
+console.log(null==0)        //false
+console.log(null>0)         //false
+console.log(null>=0)        //true
+
+console.log(undefined==0)        //false
+console.log(undefined>0)         //false
+console.log(undefined>=0)        //false
+
+console.log("2"===3)        //Strict checking (check datatype and value ) give false
