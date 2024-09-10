546053165d2ab61d6c642be87abb176a9b36a2ae
Faisal Khan <faisal5189126@gmail.com>
2024-09-10 23:17:39 +0530

    Function creation done

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,11 @@
+function greeting(name){    //name->parameters
+    console.log(`hello Mr. ${name} `)
+}
+greeting("Faisal Khan")     //Faisal Khan->arguments
+
+function add(num1,num2){
+    return num1+num2
+    console.log("added")    //this will not run as function get back from return statement
+}
+console.log(add(3,4))  
+
